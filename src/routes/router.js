75d1b248eb6f736576faa6e7b3b054927be34a13
Aleksej Petricig <minen.petricig@gmail.com>
2024-01-03 00:11:52 +0100

    Configurazione del Router Outlet.

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+
+import Root from '@pages/Root';
 import Welcome from '@pages/Welcome.js';
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Welcome/>
+    },
+    {
+        path: '/register',
+        element: <Root/>,
+        children: [
+            {
+                path: '/register',
+                element: <Register/>
+            },
+        ]
     }
 ]);
 
