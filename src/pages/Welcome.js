85b85a78d873ad637d53f926debd90eab358a33f
Aleksej Petricig <minen.petricig@gmail.com>
2024-01-03 00:16:06 +0100

    Creazione della molecola FormInput.

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Button from "@atoms/Button";
 
 function Welcome() {
@@ -6,13 +7,13 @@ function Welcome() {
       <header className="text-center">
         <img className="w-full max-w-xs mx-auto" src="/assets/CryptoWallet-logo_nobg.png" alt="Crypto Wallet Logo" />
         <h1 className="text-primary text-5xl font-bold mt-5">
-        <span className="text-secondary">C</span>rypto
+          <span className="text-secondary">C</span>rypto
           <span className="text-secondary">W</span>allet
         </h1>
       </header>
       <div className="mt-20 flex flex-col items-center justify-center gap-4">
         <Button type="primary">Log in</Button>
-        <a to="" className="uppercase text-primary text-xl">Sign up</a>
+        <Link to="/register" className="uppercase text-primary text-xl">Sign up</Link>
       </div>
     </div>
   );
